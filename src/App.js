62ec693a94f16299fd2562aa029f66c6bbd350a8
jakob-bjorner <47092630+jakob-bjorner@users.noreply.github.com>
2021-08-09 16:49:38 -0500

    changes made
    Nothing is going to be on github, why is it tracking changes???

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
+// functions with hooks tend to be the default now
 function App() {
+  // cannot use class, must use className
+  // cannot use for, must use htmlFor
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello from react</h1>
     </div>
   );
 }
