dbc93884cefaa17820a19963e8ac3b35b4f07924
jakob-bjorner <47092630+jakob-bjorner@users.noreply.github.com>
2021-08-10 15:01:45 -0500

    updating changes added button

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
 
+import Header from "./components/Header"
 // functions with hooks tend to be the default now
 function App() {
   // cannot use class, must use className
   // cannot use for, must use htmlFor
+  // can use empty brackets for no root <> </>
+
   return (
-    <div className="App">
-      <h1>Hello from react</h1>
+    <div className="container">
+      <Header title="Task Tracker" />
     </div>
   );
 }
+
 
 export default App;
